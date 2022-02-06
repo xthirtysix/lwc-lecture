692a127847cd1093c7335691e21cf66f57bac3dd
x36 <ggalliosha@gmail.com>
2022-02-06 10:38:55 +0300

    Добавляет публикацию сообщений в канал LMS, исправляет структуру данных

--- force-app/main/default/lwc/basics_navigation/basics_navigation.js
+++ force-app/main/default/lwc/basics_navigation/basics_navigation.js
@@ -1,30 +1,45 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, wire} from 'lwc'
+import {publish, MessageContext} from 'lightning/messageService'
+import NAVIGATION_CHANNEL from '@salesforce/messageChannel/basicsNavigation__c';
 
 const STEPS = [
-  {id: 1, value: 1, label: 'First'},
-  {id: 2, value: 2, label: 'Second'}
-];
+  {
+    id: 1, value: 1, label: 'First', items: [
+      {id: 1, value: 1, label: 'First'},
+      {id: 2, value: 2, label: 'Second'}
+    ]
+  },
+  {
+    id: 2, value: 2, label: 'Second', items: [
+      {id: 1, value: 1, label: 'First'},
+      {id: 2, value: 2, label: 'Second'}
+    ]
+  }
+]
 
 export default class BasicsNavigation extends LightningElement {
   chosenStepIndex = 0;
+  @wire(MessageContext)
+  messageContext;
 
   get steps() {
-    return STEPS;
+    return STEPS
   }
 
   get currentStep() {
-    return this.steps[this.chosenStepIndex].value;
+    return this.steps[this.chosenStepIndex]
   }
 
   // срабатывает при подключении компонента, и до рендеринга
   connectedCallback() {
     // добавляет стиль из дизайн-системы slds
-    this.classList.add('slds-page-header');
+    this.classList.add('slds-page-header')
     // добавляет атрибут
-    this.setAttribute('role', 'region');
+    this.setAttribute('role', 'region')
   }
 
   handleStepFocus(event) {
-    this.chosenStepIndex = event.detail.index;
+    this.chosenStepIndex = event.detail.index
+    publish(this.messageContext, NAVIGATION_CHANNEL, this.currentStep.items)
   }
 }
