855361e78f55f770de5b1b9041b0379f41d97eef
x36 <ggalliosha@gmail.com>
2022-02-06 10:48:12 +0300

    Добавляет подписку на канал сообщений LMS

--- force-app/main/default/lwc/basics_sidebar/basics_sidebar.js
+++ force-app/main/default/lwc/basics_sidebar/basics_sidebar.js
@@ -1,24 +1,55 @@
-import {LightningElement} from 'lwc';
-
-const MENU_ITEMS = [
-  {id: 1, label: 'First', name: 'first'},
-  {id: 2, label: 'Second', name: 'second'}
-]
+import {LightningElement, wire} from 'lwc'
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext,
+} from 'lightning/messageService'
+import NAVIGATION_CHANNEL from '@salesforce/messageChannel/basicsNavigation__c'
 
 export default class BasicsSidebar extends LightningElement {
+  navigationItems = []
   selectedItem = undefined
+  subscription = undefined
+  @wire(MessageContext)
+  messageContext;
 
   get items() {
-    return MENU_ITEMS
+    return this.navigationItems
   }
 
   connectedCallback() {
     this.classList.add('slds-page-header')
-    this.selectedItem = MENU_ITEMS[0].name
+    this.subscribeToMessageChannel()
+  }
+
+  disconnectedCallback() {
+    this.unsubscribeToMessageChannel()
   }
 
   handleItemSelect(event) {
     this.selectedItem = event.detail.name
+  }
+
+  handleMessage(message) {
+    this.navigationItems = message
+    this.selectedItem = message[0]?.name
+  }
+
+  subscribeToMessageChannel() {
+    if (!this.subscription) {
+      this.subscription = subscribe(
+        this.messageContext,
+        NAVIGATION_CHANNEL,
+        (message) => this.handleMessage(message),
+        {scope: APPLICATION_SCOPE}
+      );
+    }
+  }
+
+  unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
   }
 
 }
