ae987de7d256082bba3ca895bf7a6c5a44c0abba
x36 <ggalliosha@gmail.com>
2022-02-06 10:16:43 +0300

    Добавляет вертикальную навигацию в sidebar

--- force-app/main/default/lwc/basics_sidebar/basics_sidebar.js
+++ force-app/main/default/lwc/basics_sidebar/basics_sidebar.js
@@ -1,7 +1,24 @@
 import {LightningElement} from 'lwc';
 
+const MENU_ITEMS = [
+  {id: 1, label: 'First', name: 'first'},
+  {id: 2, label: 'Second', name: 'second'}
+]
+
 export default class BasicsSidebar extends LightningElement {
-  connectedCallback() {
-    this.classList.add('slds-page-header');
+  selectedItem = undefined
+
+  get items() {
+    return MENU_ITEMS
   }
+
+  connectedCallback() {
+    this.classList.add('slds-page-header')
+    this.selectedItem = MENU_ITEMS[0].name
+  }
+
+  handleItemSelect(event) {
+    this.selectedItem = event.detail.name
+  }
+
 }
